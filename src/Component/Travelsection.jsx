38aa869c648fec6f4dd68381cb538df5d41dacc2
abdelmouiz <a_youb@estin.dz>
2024-08-10 00:46:03 -1100

    50% of the project is completed

--- src/Component/Travelsection.jsx
+++ src/Component/Travelsection.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import "../App.css"
-import 
+import Thegirl from "../../Images/Thegirl.svg"
 function Travelsection() {
   return (
     <div className='travel-section'>
       <div className='travel-section-img'>
-        <img src=  alt="" />
+        <img src= {Thegirl}  alt="" />
       </div>
       <div className='travel-section-content'></div>
     </div>
